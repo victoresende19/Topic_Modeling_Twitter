98307a442b9db0bea8aa50e0ce7ef198797d2f66
Victor Resende <63743020+victoresende19@users.noreply.github.com>
2024-04-12 10:13:29 -0300

    Update vis.py

--- vis.py
+++ vis.py
@@ -14,7 +14,7 @@ conn = db.raw_connection()
 query = f'SELECT * FROM twitter_{str(suspense_bar)}'
 df = pd.read_sql_query(query, conn)
 
-fig = px.bar(df[0:11], x='termo', y='rank')
+fig = px.bar(df[0:6], x='termo', y='rank')
 fig.update_xaxes(tickfont=dict(size=15))
 st.plotly_chart(fig)
 st.write(f"<p align='justify'> Quantidade de dados: {len(df)} <p align='justify'>", unsafe_allow_html=True)
