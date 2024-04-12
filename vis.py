a387e54e91c239bf659397a93b41898c8d527c90
Victor Resende <63743020+victoresende19@users.noreply.github.com>
2024-04-12 09:32:10 -0300

    Update vis.py

--- vis.py
+++ vis.py
@@ -5,9 +5,8 @@ import streamlit as st
 
 st.set_page_config(page_icon='🦜', page_title='Tópicos Twitter', layout='centered')
 st.markdown("<h3 style='text-align: center; color: black;'> 🏎️ Assuntos Twitter: Fórmula 1 🏎️ </h3>", unsafe_allow_html=True)
-st.write("<p align='justify'> <strong>Esse projeto venceu o hackathon IBM Esteira Cognitiva 2023, utiliza o conceito de Topic Modeling.</strong> Visava utilizar a API do X (antigo Twitter) em Python para, à escolha de um tema escolhido pelo do autor, identificar os tópicos mais abordados nos tweets do respectivo tema, do qual escolheu-se por: <strong> Fórmula 1 </strong>. <p align='justify'>", unsafe_allow_html=True)
+st.write("<p align='justify'> <strong>Esse projeto venceu o hackathon IBM Esteira Cognitiva 2023, com o conceito de Topic Modeling.</strong> Visava utilizar a API do X (antigo Twitter) em Python para, à escolha de um tema escolhido pelo do autor, identificar os tópicos mais abordados nos tweets do respectivo tema, do qual escolheu-se por: <strong> Fórmula 1 </strong>. <p align='justify'>", unsafe_allow_html=True)
 st.write("<p align='justify'> O projeto foi concebido com seguinte a arquitetura: ETL para extração e aplicação de técnicas de NLP (Normalização, tokenização e lematização) visando o tratamento dos textos nos tweets em Python. Aplicação da técnica TF-IDF para a configuração dos pesos nas palavras mais relevantes do corpus em Python. Armazenamento dos resultados em unigrama, bigrama, trigrama em um banco de dados SQLite via Python. Por fim, a disponibilização dos resultados utilizando a biblioteca Streamlit em Python. <p align='justify'>", unsafe_allow_html=True)
-st.write("<p align='justify'> Por Victor Augusto Souza Resende  <p align='justify'>", unsafe_allow_html=True)
 suspense_bar = st.selectbox('Selecione tipo de visualização:', ['bigram', 'trigram'], key="1")
 
 db = create_engine(f'sqlite:///twitter_{str(suspense_bar)}.sqlite', echo=False)
@@ -18,3 +17,4 @@ df = pd.read_sql_query(query, conn)
 fig = px.bar(df[0:11], x='termo', y='rank')
 st.plotly_chart(fig)
 st.write(f"<p align='justify'> Quantidade de dados: {len(df)} <p align='justify'>", unsafe_allow_html=True)
+st.write("<p align='justify'> Por Victor Augusto Souza Resende  <p align='justify'>", unsafe_allow_html=True)
