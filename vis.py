1676c7dbe949412c289e6e0ea8d443735f164162
Victor Resende <63743020+victoresende19@users.noreply.github.com>
2024-04-11 22:55:16 -0300

    Update vis.py

--- vis.py
+++ vis.py
@@ -5,6 +5,7 @@ import streamlit as st
 
 st.set_page_config(page_icon='🦜', page_title='Tópicos Twitter', layout='centered')
 st.markdown("<h1 style='text-align: center; color: black;'> 🏎️ Twitter - Formula 1 🏎️ </h1>", unsafe_allow_html=True)
+st.write("<p align='justify'> <strong>Esse venceu o hackathon IBM Esteira Cognitiva.</strong> Visava utilizar a API do X (antigo Twitter) em Python para, à escolha de um tema a gosto do autor, identificar os tópicos mais abordados nos tweets do respectivo tema, do qual escolheu-se por: <strong> Formúla 1 </strong> <p align='justify'>", unsafe_allow_html=True)
 st.write("<p align='justify'> Por Victor Augusto Souza Resende  <p align='justify'>", unsafe_allow_html=True)
 suspense_bar = st.selectbox('Selecione tipo de visualização:', ['bigram', 'trigram'], key="1")
 
