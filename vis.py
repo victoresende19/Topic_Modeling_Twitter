6d51329dd7cc17b898ea77398dae756c315386b4
Victor Resende <63743020+victoresende19@users.noreply.github.com>
2024-04-11 23:26:34 -0300

    Update vis.py

--- vis.py
+++ vis.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 st.set_page_config(page_icon='🦜', page_title='Tópicos Twitter', layout='centered')
 st.markdown("<h1 style='text-align: center; color: black;'> 🏎️ Twitter - Fórmula 1 🏎️ </h1>", unsafe_allow_html=True)
-st.write("<p align='justify'> <strong>Esse venceu o hackathon IBM Esteira Cognitiva.</strong> Visava utilizar a API do X (antigo Twitter) em Python para, à escolha de um tema a gosto do autor, identificar os tópicos mais abordados nos tweets do respectivo tema, do qual escolheu-se por: <strong> Fórmula 1 </strong>. <p align='justify'>", unsafe_allow_html=True)
+st.write("<p align='justify'> <strong>Esse projeto venceu o hackathon IBM Esteira Cognitiva 2023.</strong> Visava utilizar a API do X (antigo Twitter) em Python para, à escolha de um tema a gosto do autor, identificar os tópicos mais abordados nos tweets do respectivo tema, do qual escolheu-se por: <strong> Fórmula 1 </strong>. <p align='justify'>", unsafe_allow_html=True)
 st.write("<p align='justify'> O projeto foi concebido após a arquitetura: ETL para extração e aplicação de técnicas de NLP (Normalização, tokenização e lematização) visando o tratamento dos textos nos tweets em Python. Aplicação da técnica TF-IDF para a configuração dos pesos nas palavras mais relevantes do corpus em Python. Armazenamento dos resultados em unigrama, bigrama, trigrama em um banco de dados SQLite via Python. Por fim, a disponibilização dos resultados utilizando a biblioteca Streamlit em Python. <p align='justify'>", unsafe_allow_html=True)
 st.write("<p align='justify'> Por Victor Augusto Souza Resende  <p align='justify'>", unsafe_allow_html=True)
 suspense_bar = st.selectbox('Selecione tipo de visualização:', ['bigram', 'trigram'], key="1")
